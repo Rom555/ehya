bf4c2fbcfd54c93772641171f9dcf15f89f621c8
Rom555 <romanzubrilin@gmail.com>
2020-08-12 02:03:04 +0300

    create: trends

--- js/main.js
+++ js/main.js
@@ -9,4 +9,16 @@ $(document).ready(function () {
   menuButton.on("click", function () {
     $(".header-right").addClass("header-right--visible");
   });
+
+  //Табы
+  var tabsItem = $(".tabs__item");
+  var cards = $(".card");
+
+  tabsItem.on("click", function () {
+    var activeCard = $(this).attr("data-target");
+    tabsItem.removeClass("tabs__item--active");
+    cards.removeClass("card--active");
+    $(activeCard).addClass("card--active");
+    $(this).addClass("tabs__item--active");
+  });
 });
