505df803b217127ee65f85e0dd63a1373baf339b
Rom555 <romanzubrilin@gmail.com>
2020-08-13 00:34:35 +0300

    create: swiper in comments

--- js/main.js
+++ js/main.js
@@ -21,4 +21,33 @@ $(document).ready(function () {
     $(activeCard).addClass("card--active");
     $(this).addClass("tabs__item--active");
   });
+
+  //Слайдер
+  var commentsSwiper = new Swiper(".comments__swiper-container", {
+    loop: true,
+    spaceBetween: 60,
+    centeredSlides: true,
+    pagination: {
+      el: ".comments__swiper-pagination",
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '"></span>';
+      },
+      bulletClass: "comments__swiper-pagination-bullet",
+      bulletActiveClass: "comments__swiper-pagination-bullet-active",
+    },
+    autoplay: {
+      delay: 7000,
+      disableOnInteraction: false,
+    },
+  });
+
+  var commentsSwiperSlide = $(".comments__swiper-slide");
+  if ($(document).width() > 768) {
+    commentsSwiperSlide.on("mouseover", function () {
+      commentsSwiper.autoplay.stop();
+    });
+    commentsSwiperSlide.on("mouseout", function () {
+      commentsSwiper.autoplay.start();
+    });
+  }
 });
